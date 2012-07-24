3847535b792e5fb2818264d1019936965a536233
Khanh <nguyenvietnamkhanh@gmail.com>
2012-07-24 21:08:12 +0700

    update code customer search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,10 @@
 class SearchesController < ApplicationController
   LIMIT = 10
-  def index
-    puts "========================", params[:region_id]
-    @classifieds = Classified.search params[:query], :with => {:region_id => params[:region_id]}
+  def index    
+    unless params[:region_id].blank?
+      @classifieds = Classified.search params[:query], :page => params[:page], :with => {:region_id => params[:region_id]}, :per_page => LIMIT
+    else
+      @classifieds = Classified.search params[:query], :page => params[:page], :per_page => LIMIT
+    end
   end
 end
