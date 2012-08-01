4e05905b112d02a475a9bad9db4bee81c3c1c2b7
Khanh <nguyenvietnamkhanh@gmail.com>
2012-08-01 21:52:53 +0700

    enable s3 for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,4 +65,17 @@ FbBgbng::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   
+  s3_settings = YAML.load_file(Rails.root.join('config', 's3.yml'))[Rails.env]
+  S3_KEY = s3_settings['access_key_id']
+  S3_SECRET_KEY = s3_settings['secret_access_key']
+  S3_BUCKET = s3_settings['bucket']
+
+if Rails.env.production? 
+  Paperclip::Attachment.default_options.merge!({
+      storage:  :s3,
+      s3_credentials: { :access_key_id => S3_KEY, :secret_access_key => S3_SECRET_KEY },
+      bucket: S3_BUCKET,
+      path: "/:class/:id/:style/:filename"
+  })
+end
 end
